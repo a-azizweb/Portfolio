6c8d167d09876abd61123b037e2b3c296b50ca4f
Ahmad Aziz <ahmadazizhaider5657@gmail.com>
2022-10-12 17:15:38 +0500

    Navbar

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from 'next/head';
+import Navbar from '../components/Navbar';
 function HomePage() {
   return (
     <div>
       <Head>
         <title>Ahmad | Front-end developer</title>
       </Head>
+
+      <Navbar />
     </div>
   );
 }
